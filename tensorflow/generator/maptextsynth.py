f1c5ce834923c32ff2b579b9c4be8de398a2bb41
Robert Sparks <robert.william.sparks@gmail.com>
2020-05-02 21:00:45 +0100

    cast to string from byte

--- tensorflow/generator/maptextsynth.py
+++ tensorflow/generator/maptextsynth.py
@@ -51,6 +51,7 @@ def get_dataset( args=None ):
 
         while True:
             caption, image = next( gen )
+            caption = str(caption)
 
             # Transform string text to sequence of indices using charset dict
             label = charset.string_to_label(caption)
